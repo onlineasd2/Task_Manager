8b9591a78ce5a58ee8dd15590eb10d4ab8851d4d
online online <mailonline95@gmail.com>
2024-05-05 15:11:31 +0500

    Добавлен интерфейс

--- Task_Manager/MainWindow.xaml.cs
+++ Task_Manager/MainWindow.xaml.cs
@@ -20,5 +20,40 @@ namespace Task_Manager
         {
             InitializeComponent();
         }
+
+        // Закрыть приложение
+        // Сделать условие вы точно хотите выйти из программы ?
+        private void btnExit(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        // Добавить Задачу
+        private void addTask(object sender, MouseEventArgs e)
+        {
+            TaskAdd taskAdd = new TaskAdd();
+            taskAdd.Show();
+        }
+
+        // Открыть калькулятор
+        private void callCalc(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc");
+        }
+
+        // Открыть статистику
+        private void openStat(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        // Перетаскивание окна
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
     }
 }
---
+++ Task_Manager/TaskAdd.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Task_Manager
+{
+    /// <summary>
+    /// Interaction logic for TaskAdd.xaml
+    /// </summary>
+    public partial class TaskAdd : Window
+    {
+        public TaskAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+    }
+}
